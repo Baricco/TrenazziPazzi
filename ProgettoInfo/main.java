f20fb73b38720071c1f80608c167d21c5dddf9ae
IlPedraz <lorenzo03.pedrazzi@gmail.com>
2021-03-04 10:43:11 +0100

    cc

--- ProgettoInfo/main.java
+++ ProgettoInfo/main.java
@@ -1,9 +1,10 @@
-import javafx.application.Application;
+
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.layout.Pane;
-
+import javafx.application.Application;
 
 
 public class Main extends Application {
